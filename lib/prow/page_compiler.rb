49c478577c28b311ae40e241c6dfe37f66114658
Kane Baccigalupi <baccigalupi@gmail.com>
2015-02-17 09:36:15 -0800

    Nested pages can be compiled

--- lib/prow/page_compiler.rb
+++ lib/prow/page_compiler.rb
@@ -1,6 +1,7 @@
 module Prow
   class PageCompiler < Struct.new(:page, :templates, :compile_dir)
     def compile
+      make_parent_directory
       File.open(path, 'w+') do |f|
         f.write(render)
       end
@@ -20,6 +21,18 @@ module Prow
 
     def layout
       templates.layout(page.layout)
+    end
+
+    def make_parent_directory
+      FileUtils.mkdir_p(page_dir) unless File.exist?(page_dir)
+    end
+
+    def page_dir
+      compile_dir + "/" + page_path
+    end
+
+    def page_path
+      page.file_name.split('/')[0..-2].join("/")
     end
 
     def path
